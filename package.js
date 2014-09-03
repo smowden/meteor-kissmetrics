8beb894a134c897a53c11334b7dac4a2211f7ff0
Christian Smorra <smorra.christian@gmail.com>
2014-09-03 12:42:27 +0200

    migrate to 0.9.0

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-    summary: "Kissmetrics Smart package"
+  name: "smowden:kissmetrics",
+  summary: "Kissmetrics Smart package",
+  version: "0.1.0",
+  git: "https://github.com/smowden/meteor-kissmetrics.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
     api.use('deps');
     api.use('accounts-base');
     api.use('underscore');
